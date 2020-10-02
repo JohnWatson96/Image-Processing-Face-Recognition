1599f7c23e662df1785b2d4f77661cbb228644cc
JohnWatson96 <John.David.Watson96@gmail.com>
2020-10-02 11:41:20 +1000

    Code with linalg error

--- face_functions.py
+++ face_functions.py
@@ -8,7 +8,7 @@ face recognition defines using dlib
 import cv2
 import dlib
 import numpy as np
-
+from scipy.spatial import Delaunay
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("../dlib-models/shape_predictor_68_face_landmarks.dat")
@@ -16,9 +16,16 @@ recogniser = dlib.face_recognition_model_v1("../dlib-models/dlib_face_recognitio
 
 
 def find(img):
+
     detections = detector(img)  # detect faces and store rectangles
+    if len(detections) == 0:
+        print("No face detected")
+        return None, None
     shape = predictor(img, detections[0])  # find the shape of the face (use first detected face)
-    return detections[0], shape
+    points = np.array(list([point.x, point.y] for point in shape.parts()))
+    bbox = [[min(points[:, 0]), min(points[:, 1])], [max(points[:, 0]), min(points[:, 1])],
+            [min(points[:, 0]), max(points[:, 1])], [max(points[:, 0]), max(points[:, 1])]]
+    return points, bbox
 
 
 def recognise(img):
@@ -27,19 +34,125 @@ def recognise(img):
     return descriptor
 
 
-def swap(img, img2):
-    pos, shape = find(img)
-    pos2, shape2 = find(img2)
+def draw(img):  # draws facial points, Delaunay triangles and bounding box
 
-    img3 = img.copy()
+    points, bbox = find(img)
 
-    subdiv = cv2.Subdiv2D((0, 0, img.shape[1], img.shape[0]))
+    if points is None or bbox is None:
+        return img
 
-    for point in shape.parts():
-        cv2.circle(img, (point.x, point.y), 2, (0, 255, 0), -1)
-        subdiv.insert((point.x, point.y))
+    triangles = Delaunay(points)
 
-    triangles = subdiv.getTriangleList()
+    cv2.line(img, tuple(bbox[0]), tuple(bbox[1]), (0, 0, 255), 2)
+    cv2.line(img, tuple(bbox[0]), tuple(bbox[2]), (0, 0, 255), 2)
+    cv2.line(img, tuple(bbox[2]), tuple(bbox[3]), (0, 0, 255), 2)
+    cv2.line(img, tuple(bbox[1]), tuple(bbox[3]), (0, 0, 255), 2)
+
+    for point in points:
+        cv2.circle(img, (point[0], point[1]), 2, (0, 255, 0), -1)
+
+    for triangle in points[triangles.simplices]:
+        cv2.line(img, tuple(triangle[0]), tuple(triangle[1]), (255, 255, 255), 1)
+        cv2.line(img, tuple(triangle[1]), tuple(triangle[2]), (255, 255, 255), 1)
+        cv2.line(img, tuple(triangle[2]), tuple(triangle[0]), (255, 255, 255), 1)
+
+    cv2.line(img, tuple(points[triangles.simplices][10][0]), tuple(points[triangles.simplices][10][1]), (255, 0, 0), 2)
+    cv2.line(img, tuple(points[triangles.simplices][10][1]), tuple(points[triangles.simplices][10][2]), (255, 0, 0), 2)
+    cv2.line(img, tuple(points[triangles.simplices][10][2]), tuple(points[triangles.simplices][10][0]), (255, 0, 0), 2)
+
+    return img
+
+
+def get_affine_transform(input_simplices, input_points, src_points):
+
+    ones = [1, 1, 1]
+    for triangle in input_simplices:
+        input_triangle = np.vstack((input_points[triangle].T, ones))
+        src_triangle = np.vstack((src_points[triangle].T, ones))
+        print("input_triangle", input_triangle)
+        print("src_triangle", src_triangle)
+        mat = np.dot(input_triangle, np.linalg.inv(src_triangle))[:2]  # dot product and inverse
+        yield mat
+
+
+def bilinear_interpolate(img, coords):
+    int_coords = np.int32(coords)
+    x0, y0 = int_coords
+    dx, dy = coords - int_coords
+
+    # 4 Neighour pixels
+    q11 = img[y0, x0]
+    q21 = img[y0, x0 + 1]
+    q12 = img[y0 + 1, x0]
+    q22 = img[y0 + 1, x0 + 1]
+
+    btm = q21.T * dx + q11.T * (1 - dx)
+    top = q22.T * dx + q12.T * (1 - dx)
+    inter_pixel = top * dy + btm * (1 - dy)
+
+    return inter_pixel.T
+
+
+def swap(src_img, input_img, input_points, input_bbox):
+
+    src_points, src_bbox = find(src_img)
+
+    if src_points is None or src_bbox is None:  # no face detected
+        print("No face to swap")
+        return src_img
+
+    result_img = src_img.copy()  # create destination face
+    input_delaunay = Delaunay(input_points)
+    triangle_affines = np.array(list(get_affine_transform(input_delaunay.simplices, input_points, src_points)))
+
+    print("triangle_affines", triangle_affines)
+
+    xmin = np.min(src_points[:, 0])
+    xmax = np.max(src_points[:, 0]) + 1
+    ymin = np.min(src_points[:, 1])
+    ymax = np.max(src_points[:, 1]) + 1
+
+    roi_coords = np.asarray([(x, y) for y in range(ymin, ymax)
+                            for x in range(xmin, xmax)], np.uint32)
+
+    print("roi_coords", roi_coords)
+
+    roi_tri_indices = input_delaunay.find_simplex(roi_coords)
+
+    print("roi_tri_indices", roi_tri_indices)
+
+    for simplex_index in range(len(input_delaunay.simplices)):
+        coords = roi_coords[roi_tri_indices == simplex_index]
+        num_coords = len(coords)
+        out_coords = np.dot(triangle_affines[simplex_index],
+                            np.vstack((coords.T, np.ones(num_coords))))
+        x, y = coords.T
+        result_img[y, x] = bilinear_interpolate(src_img, out_coords)
+
+    cv2.imshow("result_img", result_img)
+
+    return result_img
+'''    input_img = input_img[input_bbox[0][1]:input_bbox[3][1], input_bbox[0][0]:input_bbox[3][0]]  # crop using slicing
+    result_img = cv2.resize(result_img, (src_bbox[3][0] - src_bbox[0][0],
+                                       src_bbox[3][1] - src_bbox[0][1]))  # size to source
+    result_img[src_bbox[0][1]:src_bbox[3][1], src_bbox[0][0]:src_bbox[3][0]] = input_img  # paste'''
+
+# old triangle finding function that uses cv2's subdivision
+def old_draw(img):
+
+    points, bbox = find(img)
+    sub_div = cv2.Subdiv2D((bbox[0][0], bbox[0][1], bbox[3][0], bbox[3][1]))
+
+    cv2.line(img, tuple(bbox[0]), tuple(bbox[1]), (0, 0, 255), 2)
+    cv2.line(img, tuple(bbox[0]), tuple(bbox[2]), (0, 0, 255), 2)
+    cv2.line(img, tuple(bbox[2]), tuple(bbox[3]), (0, 0, 255), 2)
+    cv2.line(img, tuple(bbox[1]), tuple(bbox[3]), (0, 0, 255), 2)
+
+    for point in points:
+        cv2.circle(img, (point[0], point[1]), 2, (0, 255, 0), -1)
+        sub_div.insert((point[0], point[1]))
+
+    triangles = sub_div.getTriangleList()
 
     for triangle in triangles:
         point1 = (triangle[0], triangle[1])
@@ -50,24 +163,4 @@ def swap(img, img2):
         cv2.line(img, point2, point3, (255, 255, 255), 1)
         cv2.line(img, point3, point1, (255, 255, 255), 1)
 
-    subdiv2 = cv2.Subdiv2D((0, 0, img2.shape[1], img2.shape[0]))
-
-    for point in shape2.parts():
-        cv2.circle(img2, (point.x, point.y), 2, (0, 255, 0), -1)
-        subdiv2.insert((point.x, point.y))
-
-    triangles = subdiv2.getTriangleList()
-
-    for triangle in triangles:
-        point1 = (triangle[0], triangle[1])
-        point2 = (triangle[2], triangle[3])
-        point3 = (triangle[4], triangle[5])
-
-        cv2.line(img2, point1, point2, (255, 255, 255), 1)
-        cv2.line(img2, point2, point3, (255, 255, 255), 1)
-        cv2.line(img2, point3, point1, (255, 255, 255), 1)
-
-    #cv2.getAffineTransform()
-    #cv2.warpAffine()
-
-    return img3
+    return img
--- main.py
+++ main.py
@@ -9,35 +9,32 @@ import cv2
 import numpy as np
 import face_functions as face
 
-img = cv2.imread("John.jpg")
-img2 = cv2.imread("Damen.jpg")
+src_img = cv2.imread("John.jpg")
+input_img = cv2.imread("Damen.jpg")
 
-img = cv2.resize(img, (int(img.shape[1]/4), int(img.shape[0]/4)))
+src_img = cv2.resize(src_img, (int(src_img.shape[1]/4), int(src_img.shape[0]/4)))
+
+cv2.imshow("src_img", src_img)
+cv2.imshow("input_img", input_img)
 
 #person = face.recognise(img)
 #person2 = face.recognise(img2)
 
 cam = cv2.VideoCapture(0)
-color_green = (0, 255, 0)
-line_width = 3
 
-img2 = cv2.resize(img2, (img.shape[1], img.shape[0]))
-img3 = face.swap(img, img2)
+input_points, input_bbox = face.find(input_img)
 
-out = np.hstack((img, img2, img3))
-cv2.imshow("out", out)
+out_img = face.swap(src_img, input_img, input_points, input_bbox)
+cv2.imshow("out_img", out_img)
 cv2.waitKey(0)
 
 while False:
     ret_val, frame = cam.read()
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    #frame = face.swap(frame, input_img, input_points, input_bbox)
 
+    frame = face.draw(frame)
 
-    dets = face.find(rgb_frame)
-    for det in dets:
-        #face.swap(img, img2, (det.left(), det.top()), (det.right(), det.bottom()))
-        cv2.rectangle(frame, (det.left(), det.top()), (det.right(), det.bottom()), color_green, line_width)
     cv2.imshow('my webcam', frame)
     if cv2.waitKey(1) == 27:
         break  # esc to quit
