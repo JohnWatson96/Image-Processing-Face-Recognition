0f3df369bb8670657034cf952b38a0b886e7fc6c
Daemondust <70620594+Daemondust@users.noreply.github.com>
2020-10-02 21:34:45 +1000

    GUI tweaks

--- main.py
+++ main.py
@@ -6,48 +6,184 @@ Face recognition project
 """
 
 import cv2
-import numpy as np
+import sys
 import face_functions as face
 
-Video = False
+from PyQt5.QtWidgets import (QApplication, QMainWindow,
+                             QPushButton, QWidget,
+                             QGridLayout, QLabel, QMessageBox)
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt, pyqtSignal, QThread, pyqtSlot 
 
-src_img = cv2.imread("John.jpg")
+Video = True
+Exit_flag = False
+src_img = cv2.imread("John.jpg") 
 input_img = cv2.imread("Damen.jpg")
 
-#person = face.recognise(img)
-#person2 = face.recognise(img2)
+# -----------Video Capture--------------------#
+class Thread(QThread):
+    changePixmapIn = pyqtSignal(QImage, name = 'In')
+    changePixmapOut = pyqtSignal(QImage, name = 'Out')
+    def run(self):
+        cam = cv2.VideoCapture(0)
+        while Exit_flag == False:
+            ret, frame = cam.read()
+            if ret:
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmapIn.emit(p)
+            frame_points, frame_bbox, _ = face.find(frame)
+            frame = face.swap(frame, frame_points, frame_bbox, input_img, input_points, input_bbox)
+            #frame = face.draw(frame)
+            if ret:
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmapOut.emit(p)
+        sys.quit()   
+        
 
-cam = cv2.VideoCapture(0)
+# GUI
+class AppWindow(QMainWindow):
 
-input_points, input_bbox = face.find(input_img)
+    # Define Button Presses
+    def Button1_pressed(self):
+        print("A")
 
-if not Video:
-    src_points, src_bbox = face.find(src_img)
-    out_img = face.swap(src_img, src_points, src_bbox, input_img, input_points, input_bbox)
+    def Button2_pressed(self):
+        print("B")
 
-    src_img = face.draw(src_img)
-    input_img = face.draw(input_img)
+    def Button3_pressed(self):
+        print("C")
 
-    cv2.imshow("src_img", src_img)
-    cv2.imshow("input_img", input_img)
-    cv2.imshow("out_img", out_img)
-    cv2.imwrite("src_img.jpg", src_img)
-    cv2.imwrite("input_img.jpg", input_img)
-    cv2.imwrite("out_img.jpg", out_img)
-    cv2.waitKey(0)
+        # Initilize main window
 
-else:
-    while True:
-        ret_val, frame = cam.read()
-        frame_points, frame_bbox = face.find(frame)
-        frame = face.swap(frame, frame_points, frame_bbox, input_img, input_points, input_bbox)
+    def __init__(self, *args, **kwargs):
+        super(AppWindow, self).__init__(*args, **kwargs)
 
-        #frame = face.draw(frame)
+        # Set Title and window dimensions
+        self.setWindowTitle("Facial Recognition Project")
 
-        cv2.imshow('my webcam', frame)
-        if cv2.waitKey(1) == 27:
-            break  # esc to quit
-cv2.destroyAllWindows()
+        # -----------Buttons-------------------------#
+        # Create  buttons
+        self.Button1 = QPushButton()
+        self.Button2 = QPushButton()
+        self.Button3 = QPushButton()
+        # Label Buttons
+        self.Button1.setText('Who am I?')
+        self.Button2.setText('Swap my face!')
+        self.Button3.setText('Remember me!')
+        # Place buttons
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.Button1, 3, 3)
+        self.layout.addWidget(self.Button2, 4, 3)
+        self.layout.addWidget(self.Button3, 5, 3)
+
+        # Connect buttons
+        self.Button1.clicked.connect(self.Button1_pressed)
+        self.Button2.clicked.connect(self.Button2_pressed)
+        self.Button3.clicked.connect(self.Button3_pressed)
+        # -----------Images-------------------------#
+
+        self.labelIn = QLabel(self)
+        self.pixmapIn = QPixmap()
+        self.resize(self.pixmapIn.width(), self.pixmapIn.height())
+        self.layout.addWidget(self.labelIn, 0, 0, 2, 2)
+
+        self.labelOut = QLabel(self)
+        self.pixmapOut = QPixmap()
+        self.resize(self.pixmapOut.width(), self.pixmapOut.height())
+        self.layout.addWidget(self.labelOut, 0, 4, 2, 6)
+
+        # Lay widgets location
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
+        th = Thread(self)
+        th.changePixmapIn.connect(self.setImageIn)
+        th.changePixmapOut.connect(self.setImageOut)
+        th.start()
+        
+    # Image Update
+    @pyqtSlot(QImage, name = 'In')
+    def setImageIn(self, image):
+        # Input image
+        self.labelIn.setPixmap(QPixmap.fromImage(image))
+    
+    @pyqtSlot(QImage, name = 'Out')
+    def setImageOut(self, frame):
+        # Output image
+        self.labelOut.setPixmap(QPixmap.fromImage(frame))
+
+    def exitEvent(self, event):
+        global Exit_flag
+        ExitMsg = QMessageBox.question(self, 'Exit', 'Are you sure?', 
+                                       QMessageBox.Yes|QMessageBox.No, QMessageBox.Yes)
+        if ExitMsg == QMessageBox.Yes:
+            event.accept()
+            Exit_flag = True
+        else:
+            event.ignore()
+
+input_points, input_bbox, input_shape = face.find(input_img)
+
+input_descriptor = face.recognise(input_img, input_shape)  # WIP
+input_descriptors = [input_descriptor, input_descriptor]  # WIP
+
+
+
+    # Create Application
+
+
+app = QApplication([])
+
+# Main window
+window = AppWindow()
+
+# close the file or device
+
+
+# Display
+window.show()
+app.exec_()
+
+
+        
+
+# if not Video:
+#     src_points, src_bbox, src_shape = face.find(src_img)
+
+#     # input_descriptor = face.recognise(src_img, src_shape)  # WIP
+#     # face.match(src_descriptor, input_descriptor)  # WIP
+
+#     out_img = face.swap(src_img, src_points, src_bbox, input_img, input_points, input_bbox)
+
+#     src_img = face.draw(src_img)
+#     input_img = face.draw(input_img)
+
+#     cv2.imshow("src_img", src_img)
+#     cv2.imshow("input_img", input_img)
+#     cv2.imshow("out_img", out_img)
+#     cv2.waitKey(0)
+
+# else:
+# cam = cv2.VideoCapture(0)
+# while True:
+#         ret_val, frame = cam.read()
+#         frame_points, frame_bbox, _ = face.find(frame)
+#         frame = face.swap(frame, frame_points, frame_bbox, input_img, input_points, input_bbox)
+
+#         frame = face.draw(frame)
+
+        # cv2.imshow('my webcam', frame)
+        # if cv2.waitKey(1) == 27:
+        #     break  # esc to quit
+
 
 
 # PROJECT SCOPE
@@ -69,3 +205,6 @@ cv2.destroyAllWindows()
 # etc...
 # Lip reading (detect if talking)
 #
+
+
+
